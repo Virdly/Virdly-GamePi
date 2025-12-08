827b41f84cf9f78d12a293e443bc50b4a5479a37
Virdly <egorzvagin28sk@gmail.com>
2025-12-08 20:35:15 +0500

    Add files via upload

---
+++ Settings.h
@@ -0,0 +1,26 @@
+#define TFT_WEIGHT 240
+#define TFT_HEIGHT 240
+
+#define TFT_CS 8
+#define TFT_RST 27
+#define TFT_DC 25
+#define TFT_SCLK 10
+#define TFT_MOSI 11
+
+#define BTN_Y 9
+#define BTN_X 5
+#define BTN_A 21
+#define BTN_B 20
+
+#define BTN_START 26
+#define BTN_SELECT 19
+
+#define BTN_L 23
+#define BTN_R 14
+
+#define BTN_LEFT 16
+#define BTN_UP 15
+#define BTN_RIGHT 13
+#define BTN_DOWN 6
+
+#define BUZ 18
---
+++ VirdlyGamePi13.ino
@@ -0,0 +1,120 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7789.h>  // библиотека для дисплея ST7789
+#include <SPI.h>              // SPI подключение
+#include "GyverButton.h"
+#include "Bahamas6.h"
+#include "Settings.h"
+#include "Bitmaps.h"
+
+
+uint16_t FONE_COLOR = ST77XX_BLACK; //0xf800
+uint16_t TEXT_COLOR = ST77XX_WHITE;
+boolean AUDIOC = true;
+
+byte MainMenu = 0;
+byte MainSelect = 0;
+byte SettSelect = 0;
+
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+
+GButton buttup(BTN_UP);
+GButton butty(BTN_Y);
+GButton buttdown(BTN_DOWN);
+
+
+void setup() {
+  Serial.begin(115200);
+  tft.init(240, 240);
+  tft.setRotation(2);
+  tft.fillScreen(FONE_COLOR);
+  tft.setFont(&Bahamas6pt8b);
+  tft.setTextColor(TEXT_COLOR);
+  mainmenu();
+}
+
+void loop() {
+  buttup.tick();
+  butty.tick();
+  buttdown.tick();
+
+  //MainMenu
+  if (MainMenu == 0) {
+    if (buttup.isClick() && MainSelect > 0) {
+      MainSelect--;
+      tft.fillRect(0, 17, 19, 38 + 18 * 3, FONE_COLOR);
+      if (MainSelect == 0) {
+        tft.setCursor(0, 38);
+        tft.print(">");
+      } else {
+        tft.setCursor(0, 38 + 18 * MainSelect);
+        tft.print(">");
+      }
+    }
+    if (buttdown.isClick() && MainSelect < 3) {
+      MainSelect++;
+      tft.fillRect(0, 17, 19, 38 + 18 * 3, FONE_COLOR);
+      if (MainSelect == 0) {
+        tft.setCursor(0, 38);
+        tft.print(">");
+      } else {
+        tft.setCursor(0, 38 + 18 * MainSelect);
+        tft.print(">");
+      }
+    }
+    if (butty.isClick()) {
+      switch (MainSelect) {
+        case 0:
+          MainMenu = 1;
+          tft.fillRect(0, 0, 240, 120, FONE_COLOR);
+          settingsmenu();
+          break;
+        case 1:
+          break;
+        case 2:
+          break;
+        case 3:
+          break;
+      }
+    }
+  }
+  //Settings
+  if (MainMenu == 1) {
+    if (buttup.isClick() && SettSelect > 0) {
+      SettSelect--;
+      tft.fillRect(0, 17, 19, 38 + 18 * 3, FONE_COLOR);
+      if (SettSelect == 0) {
+        tft.setCursor(0, 38);
+        tft.print(">");
+      } else {
+        tft.setCursor(0, 38 + 18 * SettSelect);
+        tft.print(">");
+      }
+    }
+    if (buttdown.isClick() && SettSelect < 3) {
+      SettSelect++;
+      tft.fillRect(0, 17, 19, 38 + 18 * 3, FONE_COLOR);
+      if (SettSelect == 0) {
+        tft.setCursor(0, 38);
+        tft.print(">");
+      } else {
+        tft.setCursor(0, 38 + 18 * SettSelect);
+        tft.print(">");
+      }
+    }
+    if (butty.isClick()) {
+      switch (SettSelect) {
+        case 0:
+          MainMenu = 0;
+          tft.fillRect(0, 0, 240, 120, FONE_COLOR);
+          mainmenu();
+          break;
+        case 1:
+          break;
+        case 2:
+          break;
+        case 3:
+          break;
+      }
+    }
+  }
+}
