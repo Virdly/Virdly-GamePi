feb418377e6aab70a1b8f148995beba8c19acdaa
Virdly <egorzvagin28sk@gmail.com>
2025-12-08 20:58:29 +0500

    Add files via upload

--- VirdlyGamePi13.ino
+++ VirdlyGamePi13.ino
@@ -1,4 +1,40 @@
-//Loading library
+// Код ещё не полностью готов, в скором времени буду дорабатывать
+
+
+/*
+Параметры платы
+
+
+Плaта: "Waveshare RP2040 PiZero"
+
+Debug Port: "Disabled"
+
+C++ Exceptions: "Disabled"
+
+Flash Size: "16MB (Sketch: 11MB, FS: 5MB)"
+
+CPU Speed: "200 MHz"
+IP/Bluetooth Stack: "IPv4 Only"
+Optimize: "Small (-Os) (standard)"
+Operating System: "None"
+
+Profiling: "Disabled"
+
+RTTI: "Disabled"
+
+Stack Protector: "Disabled"
+
+Upload Method: "Default (UF2)"
+USB Stack: "Pico SDK"
+
+Если у вас еще не установлены платы Rasbery Pi Pico то добавте эту ссылку в "Параметры\Дополнительные ссылки для Менеджера плат"
+https://github.com/earlephilhower/arduino-pico/releases/download/global/package_rp2040_index.json
+
+*/
+
+
+
+//Загрузка библиотек
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7789.h>
 #include <SPI.h>
@@ -7,7 +43,7 @@
 #include "Settings.h"
 #include "Bitmaps.h"
 
-//Variables in the main menu, etc.
+//Переменные меню и тд
 uint16_t FONE_COLOR = ST77XX_BLACK;
 uint16_t TEXT_COLOR = ST77XX_WHITE;
 boolean AUDIOC = true;
@@ -16,7 +52,7 @@ byte MainMenu = 0;
 byte MainSelect = 0;
 byte SettSelect = 0;
 
-//Creating a screen and buttons object
+//Создаем объекты экрана и кнопок
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 
 GButton buttup(BTN_UP);
@@ -25,22 +61,23 @@ GButton buttdown(BTN_DOWN);
 
 
 void setup() {
-  Serial.begin(115200);
-  tft.init(240, 240);
-  tft.setRotation(2);
-  tft.fillScreen(FONE_COLOR);
-  tft.setFont(&Bahamas6pt8b);
-  tft.setTextColor(TEXT_COLOR);
-  mainmenu();
+  tft.init(240, 240); //Инициализируем экран
+  tft.setRotation(2); //Устанавливаем ориентацию экрана
+  tft.fillScreen(FONE_COLOR); //Закрашиваем экран
+  tft.setFont(&Bahamas6pt8b); //Устанавливаем шрифт
+  tft.setTextColor(TEXT_COLOR); //Устанавливаем цвет текста
+  mainmenu(); //Отображаем главное меню
 }
 
 void loop() {
+  //Опрашиваем кнопки
   buttup.tick();
   butty.tick();
   buttdown.tick();
 
-  //MainMenu
+  //Главное меню
   if (MainMenu == 0) {
+    //условия при нажатии кнопок
     if (buttup.isClick() && MainSelect > 0) {
       MainSelect--;
       tft.fillRect(0, 17, 19, 38 + 18 * 3, FONE_COLOR);
@@ -79,8 +116,9 @@ void loop() {
       }
     }
   }
-  //Settings
+  //Настройки
   if (MainMenu == 1) {
+    //условия при нажатии кнопок
     if (buttup.isClick() && SettSelect > 0) {
       SettSelect--;
       tft.fillRect(0, 17, 19, 38 + 18 * 3, FONE_COLOR);
